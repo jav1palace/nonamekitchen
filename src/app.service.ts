8d8d95c2458de9317841a5ff583c41e7056e2ee8
Zohane87 <62698636+Zohane87@users.noreply.github.com>
2022-09-06 17:18:17 +0200

    NNK-1 Fix the type back to string

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): number {
-    return 3;
+  getHello(): string {
+    return 'Hello World';
   }
 }
